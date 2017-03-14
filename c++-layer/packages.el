b472b870575e7e417df225c509a4e8fe031fc9bc
James Cagalawan <james.cagalawan@gmail.com>
2017-03-13 22:39:37 -0400

    Add better syntax highlighting for modern C++

--- c++-layer/packages.el
+++ c++-layer/packages.el
@@ -1,7 +1,11 @@
 (defconst c++-layer-packages
   '(
-    cc-mode))
+    cc-mode
+    modern-cpp-font-lock))
 
 (defun c++-layer/init-cc-mode ()
   (use-package cc-mode
     :mode ("\\.h\\'" . c++-mode)))
+
+(defun c++-layer/init-modern-cpp-font-lock ()
+  (add-hook 'c++-mode-hook #'modern-c++-font-lock-mode))
