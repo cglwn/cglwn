1830e4fa9762ec5b0a77b7a4218cb15e257da3bd
James Cagalawan <james.cagalawan@gmail.com>
2017-03-15 02:58:40 -0400

    Fix parsing due to trailing comma in CSV

--- sauna/plot_gps_2d.py
+++ sauna/plot_gps_2d.py
@@ -10,7 +10,7 @@ import seaborn as sns
 parser = argparse.ArgumentParser(description="Plot GPS data.")
 parser.add_argument("gps_csv", help="Path to the CSV file.")
 args = parser.parse_args()
-gps_data = pd.read_csv(args.gps_csv)
+gps_data = pd.read_csv(args.gps_csv, usecols=["id", "x", "y" ,"z"])
 
 plt.plot(gps_data["x"], gps_data["y"])
 plt.title("Top Down GPS Tracks - teach1")
